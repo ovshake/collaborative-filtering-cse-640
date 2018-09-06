ddd17973ab8b43902ffa0f1f253fc92171291a1d
ovshake <abhishek16005@iiitd.ac.in>
2018-09-06 12:20:29 +0530

    added top k similiar users feature for user based

--- UserBased.py
+++ UserBased.py
@@ -13,57 +13,84 @@ USER_AXIS = 0
 ITEM_AXIS = 1
 
 def convertFileToDataframe(filename):
-    df = pd.read_csv("../ml-100k/"+filename , sep = "\t" , names = ["User Id" , "Item Id" , "Rating" , "Timestamp"] , header = None)
-    return df  
+	df = pd.read_csv("../ml-100k/"+filename , sep = "\t" , names = ["User Id" , "Item Id" , "Rating" , "Timestamp"] , header = None)
+	return df  
 
 def convertDataframeToMatrix(df):
-    mat = np.zeros(shape=(NUM_USERS , NUM_ITEMS)) 
-    for i in range(len(df)):
-        rating = df.loc[i , RATING_COL]
-        user_id = df.loc[i , USER_ID_COL]
-        item_id = df.loc[i , ITEM_ID_COL]
-        mat[user_id - 1][item_id - 1] = rating
-    return mat 
-    
-def getPredictedRating(item , user , rating_matrix_base , cosine_similarity):
-    
-    total_cosine_similarity = 0
-    predicted_rating = 0
-    for user_id in range(rating_matrix_base.shape[USER_AXIS]):
-        if user_id != user:
-            total_cosine_similarity += cosine_similarity[user][user_id]
-
-    # print("total cosine similarity" , total_cosine_similarity)
-    for user_id in range(rating_matrix_base.shape[USER_AXIS]):
-        if user_id != user:
-            predicted_rating += (cosine_similarity[user][user_id] / total_cosine_similarity) * rating_matrix_base[user_id][item]
-    return predicted_rating
+	mat = np.zeros(shape=(NUM_USERS , NUM_ITEMS)) 
+	for i in range(len(df)):
+		rating = df.loc[i , RATING_COL]
+		user_id = df.loc[i , USER_ID_COL]
+		item_id = df.loc[i , ITEM_ID_COL]
+		mat[user_id - 1][item_id - 1] = rating
+	return mat 
+   
 
 
-def getMeanAbsoluteError(filename):
-    train_file_name = filename+".base"
-    test_file_name = filename+".test"
-    rating_matrix_base = convertDataframeToMatrix(convertFileToDataframe(train_file_name))
-    rating_matrix_test = convertDataframeToMatrix(convertFileToDataframe(test_file_name))
-    # print(rating_matrix_test[333][3])
-    cosine_similarity = 1 - pairwise_distances(rating_matrix_base , metric='cosine')
-    mean_absolute_error = 0
-    # p = getPredictedRating(3 , 333 , rating_matrix_base , cosine_similarity)
-    # print("jnbdsjdf" , p)
-    # sys.exit()
-    for user_id in range(rating_matrix_test.shape[USER_AXIS]):
-        for item_id in range(rating_matrix_base.shape[ITEM_AXIS]):
-            if rating_matrix_test[user_id][item_id] != 0:
-                mean_absolute_error += abs(getPredictedRating(item_id , user_id , rating_matrix_base , cosine_similarity) - rating_matrix_test[user_id][item_id])
-    
-    return mean_absolute_error
-mean_error = 0
-for i in range(1,6):
-    split_error = getMeanAbsoluteError("u"+str(i))
-    print("for u{} the mean abs error is {}".format(i , split_error))
-    mean_error += split_error
+def get_k_most_similiar(k , user ,cosine_similarity):
+	USER = 0
+	RATING_GIVEN = 1
+	user_tups = []
+	for user_id in range(cosine_similarity.shape[0]):
+		if user_id != user:
+			tup = (USER , cosine_similarity[user_id][user])
+			user_tups.append(tup) 
+	user_tups = sorted(user_tups , key = lambda x : x[RATING_GIVEN] , reverse = True)
+	user_tups = user_tups[:k] 
+	return user_tups
 
-print("Mean Error is {}".format(mean_error / 5))
+
+def getPredictedRating(item , user , rating_matrix_base , cosine_similarity, k = -1):
+	
+	total_cosine_similarity = 0
+	predicted_rating = 0
+	if k == -1:
+		for user_id in range(rating_matrix_base.shape[USER_AXIS]):
+			if user_id != user:
+				total_cosine_similarity += cosine_similarity[user][user_id]
+		for user_id in range(rating_matrix_base.shape[USER_AXIS]):
+			if user_id != user:
+				predicted_rating += (cosine_similarity[user][user_id] / total_cosine_similarity) * rating_matrix_base[user_id][item]
+		return predicted_rating
+	else:
+		user_tups = get_k_most_similiar(k , user , cosine_similarity)
+		total_cosine_similarity = 0
+		for user_id , cos_sim in user_tups:
+			total_cosine_similarity += cos_sim 
+		for user_id , cos_sim in user_tups:
+			predicted_rating += (cosine_similarity[user][user_id] / total_cosine_similarity) * rating_matrix_base[user_id][item]
+		return predicted_rating 
+
+
+def getMeanAbsoluteError(filename , k):
+	train_file_name = filename+".base"
+	test_file_name = filename+".test"
+	rating_matrix_base = convertDataframeToMatrix(convertFileToDataframe(train_file_name))
+	rating_matrix_test = convertDataframeToMatrix(convertFileToDataframe(test_file_name))
+	# print(rating_matrix_test[333][3])
+	cosine_similarity = 1 - pairwise_distances(rating_matrix_base , metric='cosine')
+	mean_absolute_error = 0
+	# p = getPredictedRating(473 , 602 , rating_matrix_base , cosine_similarity , k = k)
+	# #print("jnbdsjdf" , p)
+	# #sys.exit()
+	total_prediction = 0
+	for user_id in range(rating_matrix_test.shape[USER_AXIS]):
+		for item_id in range(rating_matrix_test.shape[ITEM_AXIS]):
+			if rating_matrix_test[user_id][item_id] != 0:
+				mean_absolute_error += abs(getPredictedRating(item_id , user_id , rating_matrix_base , cosine_similarity , k) - rating_matrix_test[user_id][item_id])
+				total_prediction += 1
+	
+	return mean_absolute_error / total_prediction
+
+if __name__ == "__main__":
+	k_vals = [-1, 10 , 20 , 30 , 40 , 50]
+	for k in k_vals:
+		mean_error = 0
+		for i in range(1, 6):
+			split_error = getMeanAbsoluteError("u"+str(i) , k = k)
+			print("for u{} the mean abs error is {} k = {}".format(i , split_error , k))
+			mean_error += split_error
+		print(mean_error / 5)
 
 
 
